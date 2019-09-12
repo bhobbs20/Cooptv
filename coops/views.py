a73c184fcd32545c7f3fb76c0ea6ac1b1956a172
Brian Hobbs <brianhobbs216@gmail.com>
2019-09-12 15:31:06 -0400

    Completed crud for coops

--- coops/views.py
+++ coops/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Coop
+import requests
 
 def home(request):
     return render(request, 'coops/index.html')
+
+def dashboard(request):
+    return render(request, 'coops/dashboard.html')
 
 
 class SignUp(generic.CreateView):
@@ -13,14 +18,42 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    # def form_valid(self, form):
+    #     view = super(SignUp, self).form_valid(form)
+    #     username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+    #     user = authenticate(username=username, password=password)
+    #     login(self.request, user)
+    #     return view
+
 
 class login(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
     template_name = 'registration/login.html'
+
 
 class CreateCoop(generic.CreateView):
     model = Coop 
     fields = ['title']
     template_name = 'coops/create_coop.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateCoop, self).form_valid(form)
+        return redirect('dashboard')
+
+class DetailCoop(generic.DetailView):
+    model = Coop
+    template_name = 'coops/detail_coop.html'
+
+class UpdateCoop(generic.UpdateView):
+    model = Coop
+    template_name = 'coops/update_coop.html'
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+
+class DeleteCoop(generic.DeleteView):
+    model = Coop
+    template_name = 'coops/delete_coop.html'
+    success_url = reverse_lazy('dashboard')
--- coopstube/urls.py
+++ coopstube/urls.py
@@ -7,12 +7,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
+    path('dashboard', views.dashboard, name='dashboard'), 
     #Auth
     path('signup', views.SignUp.as_view(), name="signup"),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name="logout"),
     #Coops
-    path('coop/create', views.CreateCoop.as_view(), name='create_coop')
+    path('coop/create', views.CreateCoop.as_view(), name='create_coop'),
+    path('coop/<int:pk>', views.DetailCoop.as_view(), name='detail_coop'),
+    path('coop/<int:pk>/update', views.UpdateCoop.as_view(), name='update_coop'),
+    path('coop/<int:pk>/delete', views.DeleteCoop.as_view(), name='delete_coop'),
 
 ] 
